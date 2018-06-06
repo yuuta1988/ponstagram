fc09ee1a411bffc06c2826f554fc36523c79fe54
YUTA <sawamura1009@gmail.com>
2018-06-06 22:04:27 +0900

    fix migration filr

--- db/migrate/20180520092451_remove_images_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImagesFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :images, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180520092818) do
+ActiveRecord::Schema.define(version: 20180606130312) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
